6e449055d253a170afb4dd491e95bf3cd659c357
Konrad Lames <konrad.lames@stud.htwk-leipzig.de>
2024-06-07 00:27:45 +0200

    Streamlined Project.cs to be similar to Plugin.cs in branch feature/getProjectPlugins

--- ProjectMetadataPlatform.Domain/Projects/Project.cs
+++ ProjectMetadataPlatform.Domain/Projects/Project.cs
@@ -4,27 +4,12 @@ namespace ProjectMetadataPlatform.Domain.Projects;
 /// The representation of a project in the Database.
 /// </summary>
 // TODO Add list of plugins as property.
-public class Project()
+/// <summary>
+/// The representation of a project in the Database.
+/// </summary>
+// TODO Add list of plugins as property.
+public class Project
 {
-    /// <summary>
-    /// Project properties.
-    /// </summary>
-    /// <param name="id">The id of the project.</param>
-    /// <param name="projectName">The name of the project.</param>
-    /// <param name="clientName">The name of the client.</param>
-    /// <param name="businessUnit">The Business Units id.</param>
-    /// <param name="teamNumber">The team responsible for the project.</param>
-    /// <param name="department">The departments id.</param>
-    public Project(int id, string projectName, string clientName, string businessUnit, int teamNumber, string department) : this()
-    {
-        Id = id;
-        ProjectName = projectName;
-        ClientName = clientName;
-        BusinessUnit = businessUnit;
-        TeamNumber = teamNumber;
-        Department = department;
-    }
-
     /// <summary>
     /// Gets or sets the id of the project. This property is required.
     /// </summary>
@@ -36,12 +21,12 @@ public class Project()
     public required string ProjectName { get; set; }
 
     /// <summary>
-    /// Gets or sets the name of the client associated with the project. This property is nullable.
+    /// Gets or sets the name of the client associated with the project. This property is required.
     /// </summary>
     public required string ClientName { get; set; }
 
     /// <summary>
-    /// Gets or sets the business unit associated with the project. This property is nullable.
+    /// Gets or sets the business unit associated with the project. This property is required.
     /// </summary>
     public required string BusinessUnit { get; set; }
 
@@ -51,7 +36,7 @@ public class Project()
     public required int TeamNumber { get; set; }
 
     /// <summary>
-    /// Gets or sets the department associated with the project. This property is nullable.
+    /// Gets or sets the department associated with the project. This property is required.
     /// </summary>
     public required string Department { get; set; }
 }
