eabee76c27abfa653d5d21911494af871e6291c8
Carl-Moritz von Butler <carl-moritz.von_butler@stud.htwk-leipzig.de>
2024-06-08 11:14:27 +0200

    comments

--- ProjectMetadataPlatform.Application/Projects/GetProjectsBySearchingQuery.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-using MediatR;
-using ProjectMetadataPlatform.Domain.Projects;
-
-namespace ProjectMetadataPlatform.Application.Projects;
-
-public record GetProjectsBySearchingQuery(string search): IRequest<IEnumerable<Project>>;
