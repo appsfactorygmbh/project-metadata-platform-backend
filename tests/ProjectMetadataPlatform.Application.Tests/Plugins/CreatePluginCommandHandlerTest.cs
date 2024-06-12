104803792a5113f2344c851210254c0f26f1ae6d
Misha Justen <misha.justen@stud.htwk-leipzig.de>
2024-06-12 20:51:48 +0200

    add CreatePluginCommandHandler test (Pairprogramming: Tristan Sopauschke, Konrad Lames, Sebastian Schekahn, Kilian Schneider)

---
+++ tests/ProjectMetadataPlatform.Application.Tests/Plugins/CreatePluginCommandHandlerTest.cs
@@ -0,0 +1,35 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using ProjectMetadataPlatform.Application.Interfaces;
+using ProjectMetadataPlatform.Application.Plugins;
+using ProjectMetadataPlatform.Domain.Plugins;
+
+namespace ProjectMetadataPlatform.Application.Tests.Plugins;
+
+[TestFixture]
+public class CreatePluginCommandHandlerTest
+{
+    private CreatePluginCommandHandler _handler;
+    private Mock<IPluginRepository> _mockPluginRepo;
+
+    [SetUp]
+    public void Setup()
+    {
+        _mockPluginRepo = new Mock<IPluginRepository>();
+        _handler = new CreatePluginCommandHandler(_mockPluginRepo.Object);
+    }
+    
+    [Test]
+    public async Task CreatePlugin_Test()
+    {
+        var examplePlugin = new Plugin { PluginName = "Airlock", Id = 13, ProjectPlugins = [] };
+        _mockPluginRepo.Setup(m => m.CreatePlugin(It.IsAny<string>())).ReturnsAsync(examplePlugin);
+
+        var result = await _handler.Handle(new CreatePluginCommand("Airlock"), It.IsAny<CancellationToken>());
+
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.PluginName, Is.EqualTo("Airlock"));
+    }
+}
