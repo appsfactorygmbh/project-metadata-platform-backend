aa7c902b1e02c31295bde9c84a1fabffff9234cc
Finn Wulfert <finn.wulfert@stud.htwk-leipzig.de>
2024-06-14 13:16:33 +0200

    fix code smell

--- ProjectMetadataPlatform.Infrastructure/DataAccess/ProjectsRepository.cs
+++ ProjectMetadataPlatform.Infrastructure/DataAccess/ProjectsRepository.cs
@@ -2,10 +2,8 @@ using ProjectMetadataPlatform.Application.Interfaces;
 using ProjectMetadataPlatform.Domain.Projects;
 using System.Collections.Generic;
 using System.Linq;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-
 namespace ProjectMetadataPlatform.Infrastructure.DataAccess;
 
 /// <summary>
