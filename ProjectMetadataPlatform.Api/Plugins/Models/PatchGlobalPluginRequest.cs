12b3ca3eaafb81b009caed6b1eabeba82886bd2d
Kilian Schneider <kilian.schneider@stud.htwk-leipzig.de>
2024-06-29 15:02:39 +0200

    add Keys to Patch request body but don't pass it through the other layers for now

--- ProjectMetadataPlatform.Api/Plugins/Models/PatchGlobalPluginRequest.cs
+++ ProjectMetadataPlatform.Api/Plugins/Models/PatchGlobalPluginRequest.cs
@@ -5,4 +5,5 @@ namespace ProjectMetadataPlatform.Api.Plugins.Models;
 /// </summary>
 /// <param name="PluginName">The name of the plugin. Null if not being updated.</param>
 /// <param name="IsArchived">The archived status of the plugin. Null if not being updated.</param>
-public record PatchGlobalPluginRequest(string? PluginName = null, bool? IsArchived = null);
+/// <param name="Keys">An array of keys associated with the plugin.</param>
+public record PatchGlobalPluginRequest(string? PluginName = null, bool? IsArchived = null, string[]? Keys = null);
