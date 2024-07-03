ba484a91e30bb5b0de7680aea303895f7dcc00ce
Philipp Breske <philipp.breske@stud.htwk-leipzig.de>
2024-07-03 18:16:51 +0200

    code quality changes

--- ProjectMetadataPlatform.Api/Plugins/Models/GetPluginResponse.cs
+++ ProjectMetadataPlatform.Api/Plugins/Models/GetPluginResponse.cs
@@ -6,4 +6,5 @@ namespace ProjectMetadataPlatform.Api.Plugins.Models;
 /// <param name="PluginName">The name of the plugin.</param>
 /// <param name="Url">The URL of this plugin instance in the project.</param>
 /// <param name="DisplayName">The name of this plugin instance in the project.</param>
+/// <param name="Id">The global id of the plugin instance in the project.</param>
 public record GetPluginResponse(string PluginName, string Url, string DisplayName, int Id);
--- ProjectMetadataPlatform.Infrastructure/DataAccess/ProjectsRepository.cs
+++ ProjectMetadataPlatform.Infrastructure/DataAccess/ProjectsRepository.cs
@@ -1,9 +1,7 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Query;
 using ProjectMetadataPlatform.Application.Interfaces;
 using ProjectMetadataPlatform.Domain.Plugins;
 using ProjectMetadataPlatform.Domain.Projects;
