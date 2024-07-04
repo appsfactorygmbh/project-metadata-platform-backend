645ddbd4468e719e2427df94cb33b6d93f9862b7
Philipp Breske <philipp.breske@stud.htwk-leipzig.de>
2024-07-04 16:12:48 +0200

    add test in Controller for id not found

--- tests/ProjectMetadataPlatform.Api.Tests/Projects/PutProjectControllerTest.cs
+++ tests/ProjectMetadataPlatform.Api.Tests/Projects/PutProjectControllerTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
@@ -59,8 +60,16 @@ public class PutProjectControllerTest
     public async Task ChangeProjectDataControllerTest()
     {
         _mediator.Setup(m => m.Send(It.IsAny<UpdateProjectCommand>(), It.IsAny<CancellationToken>())).ReturnsAsync(1);
-        var request = new CreateProjectRequest("Example Project", "Example Business Unit", 1, "Example Department", "Example Client",[]);
-        ActionResult<CreateProjectResponse> result = await _controller.Put(request, 100);
-        Assert.That(result.Result, Is.InstanceOf<CreatedResult>());
+        var request = new UpdateProjectCommand("Example Project", "Example Business Unit", 1, "Example Department", "Example Client",1,[]);
+        var result = await _mediator.Object.Send(request);
+        Assert.That(result, Is.EqualTo(1));
+    }
+
+    [Test]
+    public async Task ChangeProjectIdNotFoundControllerTest()
+    {
+        _mediator.Setup(m => m.Send(It.IsAny<UpdateProjectCommand>(), It.IsAny<CancellationToken>())).ThrowsAsync(new InvalidOperationException("Project does not exist."));
+        var request = new UpdateProjectCommand("Example Project", "Example Business Unit", 1, "Example Department", "Example Client",2,[]);
+        Assert.That(() => _mediator.Object.Send(request), Throws.Exception.TypeOf<InvalidOperationException>());
     }
 }
