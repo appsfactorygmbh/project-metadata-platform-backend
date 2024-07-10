a717e5d99885e3b05da0b0938e2c5758207164d7
Benjamin Killisch <benjamin_uwe.killisch@stud.htwk-leipzig.de>
2024-07-10 18:10:45 +0200

    fix readme and shell command

--- ProjectMetadataPlatform.Api/dotnet_ef.sh
+++ ProjectMetadataPlatform.Api/dotnet_ef.sh
@@ -4,4 +4,4 @@
 # sh ./dotnet_ef.sh <my_parameters>
 # Example:
 # sh ./dotnet_ef.sh migrations list
-PMP_DB_URL=localhost; PMP_DB_PORT=5432; PMP_DB_NAME=pmp_db_local; PMP_DB_USER=postgres; PMP_DB_PASSWORD=postgres; dotnet ef -p ..\ProjectMetadataPlatform.Infrastructure\ -s . $*
+PMP_DB_URL=localhost PMP_DB_PORT=5432 PMP_DB_NAME=pmp_db_local PMP_DB_USER=postgres PMP_DB_PASSWORD=postgres dotnet ef -p ../ProjectMetadataPlatform.Infrastructure/ -s . $*
