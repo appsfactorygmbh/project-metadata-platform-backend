fdd10f6fbb0614c330e1d6afc0e7767ce03cc5cb
Philipp Breske <philipp.breske@stud.htwk-leipzig.de>
2024-07-11 17:11:53 +0200

    begin testing

---
+++ tests/ProjectMetadataPlatform.Api.Tests/Auth/AuthControllerTest.cs
@@ -0,0 +1,42 @@
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using ProjectMetadataPlatform.Api.Auth;
+using ProjectMetadataPlatform.Api.Auth.Models;
+using ProjectMetadataPlatform.Application.Auth;
+using ProjectMetadataPlatform.Domain.Auth;
+
+namespace ProjectMetadataPlatform.Api.Tests.Auth;
+
+public class Tests
+{
+    private AuthController _controller;
+    private Mock<IMediator> _mediator;
+    [SetUp]
+    public void Setup()
+    {
+        _mediator = new Mock<IMediator>();
+        _controller = new AuthController(_mediator.Object);
+    }
+
+    [Test]
+    public async Task CorrectLoginTest()
+    {
+        _mediator.Setup(m => m.Send(It.IsAny<LoginQuery>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new JwtTokens{AccessToken = "accessToken", RefreshToken = "refreshToken"});
+
+        var request = new LoginRequest("username", "password");
+
+        var result = await _controller.Put(request);
+        Assert.That(result.Value, Is.InstanceOf<LoginResponse>());
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.Value.accessToken, Is.EqualTo("accessToken"));
+            Assert.That(result.Value.refreshToken, Is.EqualTo("refreshToken"));
+        });
+        ;
+    }
+}
