ad3ac80c226c1ecee8a6dcfda55e8125bb7eac3c
Finn Wulfert <finn.wulfert@stud.htwk-leipzig.de>
2024-07-13 18:06:32 +0200

    add env variables

--- ProjectMetadataPlatform.Api/dotnet_ef.sh
+++ ProjectMetadataPlatform.Api/dotnet_ef.sh
@@ -4,5 +4,5 @@
 # sh ./dotnet_ef.sh <my_parameters>
 # Example:
 # sh ./dotnet_ef.sh migrations list
-PMP_DB_URL=localhost PMP_DB_PORT=5432 PMP_DB_NAME=pmp_db_local PMP_DB_USER=postgres PMP_DB_PASSWORD=postgres dotnet ef -p ../ProjectMetadataPlatform.Infrastructure/ -s . $*
+PMP_DB_URL=localhost PMP_DB_PORT=5432 PMP_DB_NAME=pmp_db_local PMP_DB_USER=postgres PMP_DB_PASSWORD=postgres JWT_VALID_ISSUER = validIssuer JWT_VALID_AUDIENCE = validAudience JWT_ISSUER_SIGNING_KEY = superSecretKeyThatIsAtLeast257BitLong@345  dotnet ef -p ../ProjectMetadataPlatform.Infrastructure/ -s . $*
 
