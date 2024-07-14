f80333301179d425bd36eb481adbb466c4cbd665
Sebastian Schekahn <sebastian.schekahn@stud.htwk-leipzig.de>
2024-07-14 14:40:36 +0200

    add docs

--- ProjectMetadataPlatform.Application/Projects/GetAllBusinessUnitsQuery.cs
+++ ProjectMetadataPlatform.Application/Projects/GetAllBusinessUnitsQuery.cs
@@ -3,4 +3,7 @@ using MediatR;
 
 namespace ProjectMetadataPlatform.Application.Projects;
 
+/// <summary>
+/// Represents a query to retrieve all business units within the system.
+/// </summary>
 public record GetAllBusinessUnitsQuery() : IRequest<IEnumerable<string>>;
--- ProjectMetadataPlatform.Application/Projects/GetAllBusinessUnitsQueryHandler.cs
+++ ProjectMetadataPlatform.Application/Projects/GetAllBusinessUnitsQueryHandler.cs
@@ -6,16 +6,33 @@ using ProjectMetadataPlatform.Application.Interfaces;
 
 namespace ProjectMetadataPlatform.Application.Projects;
 
-public class GetAllBusinessUnitsQueryHandler: IRequestHandler<GetAllBusinessUnitsQuery, IEnumerable<string>>
+/// <summary>
+/// Handles the retrieval of all business units within the system.
+/// </summary>
+/// <remarks>
+/// This handler is part of the CQRS pattern implementation using MediatR. It processes
+/// <see cref="GetAllBusinessUnitsQuery"/> to fetch a list of all business units.
+/// Utilizes the <see cref="IProjectsRepository"/> for data access.
+/// </remarks>
+public class GetAllBusinessUnitsQueryHandler : IRequestHandler<GetAllBusinessUnitsQuery, IEnumerable<string>>
 {
-    private readonly IProjectsRepository _projectsRepository;
+    private readonly IProjectsRepository _projectsRepository; // Repository interface for accessing projects data.
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GetAllBusinessUnitsQueryHandler"/> class.
+    /// </summary>
+    /// <param name="projectsRepository">The projects repository.</param>
     public GetAllBusinessUnitsQueryHandler(IProjectsRepository projectsRepository)
     {
         _projectsRepository = projectsRepository;
-
     }
 
+    /// <summary>
+    /// Handles the <see cref="GetAllBusinessUnitsQuery"/> request.
+    /// </summary>
+    /// <param name="request">The query request.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains a list of all business units.</returns>
     public Task<IEnumerable<string>> Handle(GetAllBusinessUnitsQuery request, CancellationToken cancellationToken)
     {
         return _projectsRepository.GetBusinessUnitsAsync();
