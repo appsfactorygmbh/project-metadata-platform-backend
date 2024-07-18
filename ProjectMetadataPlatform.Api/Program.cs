87b4aee7386cde04fffa7cf241ed7f95b7a0cf0d
Carl-Moritz von Butler <carl-moritz.von_butler@stud.htwk-leipzig.de>
2024-07-18 13:54:38 +0200

    changes

--- ProjectMetadataPlatform.Api/Program.cs
+++ ProjectMetadataPlatform.Api/Program.cs
@@ -44,6 +44,8 @@ builder.Services
     .AddApplicationDependencies()
     .AddInfrastructureDependencies();
 
+builder.Services.AddHttpContextAccessor();
+
 builder.Services.AddCors(options
     => options.AddDefaultPolicy(policyBuilder => policyBuilder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
 builder.Services.AddControllers();
