a7f461206450c71b838390ff10ade75bd8f711c5
Benjamin Killisch <benjamin_uwe.killisch@stud.htwk-leipzig.de>
2024-11-05 15:47:01 +0000

    add integration tests for project creation

--- tests/ProjectMetadataPlatform.IntegrationTests/BasicFunctionalities.cs
+++ tests/ProjectMetadataPlatform.IntegrationTests/BasicFunctionalities.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using FluentAssertions;
 using NUnit.Framework;
+using ProjectMetadataPlatform.IntegrationTests.Utilities;
 
 namespace ProjectMetadataPlatform.IntegrationTests;
 
