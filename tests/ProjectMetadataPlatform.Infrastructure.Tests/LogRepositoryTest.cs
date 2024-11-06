fb683bf9ea4bdb285f3c0318e96bd03c22ad4972
Misha Justen <misha.justen@stud.htwk-leipzig.de>
2024-11-06 19:06:47 +0100

    adjust Test for log repo (pairprogramming Kilian Schneider)

--- tests/ProjectMetadataPlatform.Infrastructure.Tests/LogRepositoryTest.cs
+++ tests/ProjectMetadataPlatform.Infrastructure.Tests/LogRepositoryTest.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Security.Claims;
 using System.Security.Principal;
 using System.Threading.Tasks;
@@ -5,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using NUnit.Framework;
+using ProjectMetadataPlatform.Application.Interfaces;
 using ProjectMetadataPlatform.Domain.Logs;
 using ProjectMetadataPlatform.Domain.Projects;
 using ProjectMetadataPlatform.Infrastructure.DataAccess;
@@ -17,6 +19,8 @@ public class LogRepositoryTest : TestsWithDatabase
 {
     private ProjectMetadataPlatformDbContext _context;
     private LogRepository _loggingRepository;
+    private Mock<IUsersRepository> _mockUserRepository;
+
     [SetUp]
     public void Setup()
     {
@@ -25,15 +29,23 @@ public class LogRepositoryTest : TestsWithDatabase
 
         var identity = new GenericIdentity("camo", "test");
         var contextUser = new ClaimsPrincipal(identity); //add claims as needed
-        var httpContext = new DefaultHttpContext() {
+        var httpContext = new DefaultHttpContext
+        {
             User = contextUser
         };
         httpContextAccessorMock.Setup(_ => _.HttpContext).Returns(httpContext);
-        _loggingRepository = new LogRepository(_context, httpContextAccessorMock.Object);
 
-        _context = DbContext();
+        _mockUserRepository = new Mock<IUsersRepository>();
+
+        _loggingRepository = new LogRepository(_context, httpContextAccessorMock.Object, _mockUserRepository.Object);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
         ClearData(_context);
     }
+
     [Test]
     public async Task CreateLogTest()
     {
@@ -47,13 +59,55 @@ public class LogRepositoryTest : TestsWithDatabase
         };
         await _context.Projects.AddAsync(exampleProject);
         await _context.SaveChangesAsync();
-        await _loggingRepository.AddLogForCurrentUser( exampleProject.Id, Action.ADDED, "added project");
-        var dbLog = await _context?.Logs.FirstOrDefaultAsync()!;
-        Assert.NotNull(dbLog);
+
+        var logChanges = new List<LogChange>
+        {
+            new()
+            {
+                OldValue = "",
+                NewValue = "Example Project",
+                Property = "ProjectName"
+            },
+            new()
+            {
+                OldValue = "",
+                NewValue = "Example Business Unit",
+                Property = "BusinessUnit"
+            },
+            new()
+            {
+                OldValue = "",
+                NewValue = "1",
+                Property = "TeamNumber"
+            },
+            new()
+            {
+                OldValue = "",
+                NewValue = "Example Department",
+                Property = "Department"
+            },
+            new()
+            {
+                OldValue = "",
+                NewValue = "Example Client",
+                Property = "ClientName"
+            }
+        };
+
+        await _loggingRepository.AddLogForCurrentUser( exampleProject.Id, Action.ADDED_PROJECT, logChanges);
+        var dbLog = await _context.Logs.Include(log => log.User).Include(log => log.Project).Include(log => log.Changes)
+            .FirstOrDefaultAsync()!;
+        Assert.That(dbLog, Is.Not.Null);
         Assert.Multiple(() =>
         {
-            Assert.That(dbLog.Action, Is.EqualTo(Action.ADDED));
+            Assert.That(dbLog.Id, Is.EqualTo(1));
+            Assert.That(dbLog.Action, Is.EqualTo(Action.ADDED_PROJECT));
             Assert.That(dbLog.Username, Is.EqualTo("camo"));
+            Assert.That(dbLog.UserId, Is.Null); // TODO change to actual user id once the user is set by the log repo
+            Assert.That(dbLog.User, Is.Null);
+            Assert.That(dbLog.ProjectId, Is.EqualTo(exampleProject.Id));
+            Assert.That(dbLog.Project, Is.EqualTo(exampleProject));
+            Assert.That(dbLog.Changes, Has.Count.EqualTo(5));
         });
     }
 }
