11baca84a02eeead4e8f50196d18e7cc985597e8
Misha Justen <misha.justen@stud.htwk-leipzig.de>
2024-11-06 18:51:37 +0100

    adjust LogRepo (pairprogramming Kilian Schneider)

--- ProjectMetadataPlatform.Infrastructure/Logs/LogRepository.cs
+++ ProjectMetadataPlatform.Infrastructure/Logs/LogRepository.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using ProjectMetadataPlatform.Application.Interfaces;
 using ProjectMetadataPlatform.Domain.Logs;
+using ProjectMetadataPlatform.Domain.User;
 using ProjectMetadataPlatform.Infrastructure.DataAccess;
 using static System.DateTimeOffset;
 using Action = ProjectMetadataPlatform.Domain.Logs.Action;
@@ -16,16 +18,18 @@ public class LogRepository : RepositoryBase<Log>, ILogRepository
 {
     private readonly ProjectMetadataPlatformDbContext _context;
     private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly IUsersRepository _usersRepository;
 
     /// <summary>
     ///     initialising context and httpContextAccessor to provide user information
     /// </summary>
     /// <param name="dbContext"></param>
     /// <param name="httpContextAccessor"></param>
-    public LogRepository(ProjectMetadataPlatformDbContext dbContext, IHttpContextAccessor httpContextAccessor) : base(dbContext)
+    public LogRepository(ProjectMetadataPlatformDbContext dbContext, IHttpContextAccessor httpContextAccessor, IUsersRepository usersRepository) : base(dbContext)
     {
         _context = dbContext;
         _httpContextAccessor = httpContextAccessor;
+        _usersRepository = usersRepository;
     }
 
     /// <summary>
@@ -34,19 +38,23 @@ public class LogRepository : RepositoryBase<Log>, ILogRepository
     /// <param name="projectId"></param>
     /// <param name="action"></param>
     /// <param name="changes"></param>
-    public async Task AddLogForCurrentUser(int projectId, Action action, string changes)
+    public async Task AddLogForCurrentUser(int projectId, Action action, List<LogChange> changes)
     {
-        // var username = _httpContextAccessor.HttpContext?.User.Identity?.Name ?? "Unknown user";
-        // var log = new Log
-        // {
-        //     Username = username,
-        //     Action = action,
-        //     Changes = changes,
-        //     ProjectId = projectId,
-        //     TimeStamp = UtcNow
-        // };
-        // var projects = await _context.Projects.Include(b => b.Logs).FirstAsync(pro => pro.Id == projectId);
-        // projects.Logs!.Add(log);
-        // _ = await _context.SaveChangesAsync();
+        var username = _httpContextAccessor.HttpContext?.User.Identity?.Name ?? "Unknown user";
+        // User? user = await _usersRepository.GetUserByUserNameAsync(username);
+        User? user = null;
+
+        var log = new Log
+        {
+            Username = username,
+            UserId = user?.Id,
+            Action = action,
+            ProjectId = projectId,
+            TimeStamp = UtcNow,
+            Changes = changes
+        };
+        var projects = await _context.Projects.Include(b => b.Logs).FirstAsync(pro => pro.Id == projectId);
+        projects.Logs!.Add(log);
+        _ = await _context.SaveChangesAsync();
     }
 }
