a92877c436d9262d13139f5d28578c49296094a6
Finn Wulfert <finn.wulfert@stud.htwk-leipzig.de>
2024-11-12 11:55:54 +0000

    Revert "Log now is created when adding project;"
    This reverts commit 64ac22273a92b6007ffe79662546c570d6e301ff

--- ProjectMetadataPlatform.Application/Projects/CreateProjectCommandHandler.cs
+++ ProjectMetadataPlatform.Application/Projects/CreateProjectCommandHandler.cs
@@ -1,10 +1,8 @@
 using System;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using ProjectMetadataPlatform.Application.Interfaces;
-using ProjectMetadataPlatform.Domain.Logs;
 using ProjectMetadataPlatform.Domain.Projects;
 
 namespace ProjectMetadataPlatform.Application.Projects;
@@ -16,18 +14,15 @@ public class CreateProjectCommandHandler : IRequestHandler<CreateProjectCommand,
 {
     private readonly IProjectsRepository _projectsRepository;
     private readonly IPluginRepository _pluginRepository;
-    private readonly ILogRepository _logRepository;
     /// <summary>
     ///     Creates a new instance of <see cref="CreateProjectCommandHandler" />.
     /// </summary>
     /// <param name="projectsRepository"></param>
     /// <param name="pluginRepository"></param>
-    /// <param name="logRepository"></param>
-    public CreateProjectCommandHandler(IProjectsRepository projectsRepository, IPluginRepository pluginRepository, ILogRepository logRepository)
+    public CreateProjectCommandHandler(IProjectsRepository projectsRepository, IPluginRepository pluginRepository)
     {
         _projectsRepository = projectsRepository;
         _pluginRepository = pluginRepository;
-        _logRepository = logRepository;
     }
     /// <summary>
     ///     Handles the request to create a project.
@@ -46,23 +41,6 @@ public class CreateProjectCommandHandler : IRequestHandler<CreateProjectCommand,
         }
         var project = new Project{ProjectName=request.ProjectName, BusinessUnit=request.BusinessUnit, TeamNumber=request.TeamNumber, Department=request.Department, ClientName=request.ClientName, ProjectPlugins = request.Plugins};
         await _projectsRepository.Add(project);
-        var changes = new List<LogChange>
-        {
-            new() { OldValue = "", NewValue = project.ProjectName, Property = "ProjectName" },
-            new() { OldValue = "", NewValue = project.BusinessUnit, Property = "BusinessUnit" },
-            new() { OldValue = "", NewValue = project.Department, Property = "Department" },
-            new() { OldValue = "", NewValue = project.ClientName, Property = "ClientName" }
-        };
-        if(project.TeamNumber != 0)
-        {
-            changes.Add(new LogChange { OldValue = "", NewValue = project.TeamNumber.ToString(), Property = "TeamNumber" });
-        }
-
-        foreach (var plugin in project.ProjectPlugins)
-        {
-            changes.Add(new LogChange { OldValue = "", NewValue = plugin.DisplayName, Property = "PluginDisplayName" });
-        }
-        await _logRepository.AddLogForCurrentUser(project.Id, Domain.Logs.Action.ADDED_PROJECT, changes);
         return project.Id;
     }
 }
