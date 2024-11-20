ab92672a24ad2f0aa33e2bcf1015a12f157a7c38
Finn Wulfert <finn.wulfert@stud.htwk-leipzig.de>
2024-11-20 14:25:01 +0100

    Fix Code Quality;

--- ProjectMetadataPlatform.Infrastructure/Logs/LogRepository.cs
+++ ProjectMetadataPlatform.Infrastructure/Logs/LogRepository.cs
@@ -1,7 +1,5 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
