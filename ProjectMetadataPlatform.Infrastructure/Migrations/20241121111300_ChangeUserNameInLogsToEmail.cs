200187627ca49194e0486f8586d9e98537522274
Finn Wulfert <finn.wulfert@stud.htwk-leipzig.de>
2024-11-21 12:16:32 +0100

    Add new Migration

---
+++ ProjectMetadataPlatform.Infrastructure/Migrations/20241121111300_ChangeUserNameInLogsToEmail.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectMetadataPlatform.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeUserNameInLogsToEmail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Logs",
+                newName: "Email");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Logs",
+                newName: "Username");
+        }
+    }
+}
