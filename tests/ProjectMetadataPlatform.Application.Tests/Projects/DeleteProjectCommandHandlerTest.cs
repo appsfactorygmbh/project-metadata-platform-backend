fee9a28f308207fe59556d481d2f26da01ad67c4
Misha Justen <misha.justen@stud.htwk-leipzig.de>
2024-11-28 13:11:32 +0100

    changed tests according to code review

--- tests/ProjectMetadataPlatform.Application.Tests/Projects/DeleteProjectCommandHandlerTest.cs
+++ tests/ProjectMetadataPlatform.Application.Tests/Projects/DeleteProjectCommandHandlerTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Moq;
@@ -48,8 +49,6 @@ public class DeleteProjectCommandHandlerTest
         var result = await _handler.Handle(new DeleteProjectCommand(1), It.IsAny<CancellationToken>());
 
         Assert.That(project, Is.EqualTo(result));
-
-        _mockLogRepo.Verify(m => m.AddProjectLogForCurrentUser(It.IsAny<Project>(), Action.REMOVED_PROJECT,It.IsAny<List<LogChange>>()), Times.Once);
     }
 
     [Test]
@@ -78,6 +77,35 @@ public class DeleteProjectCommandHandlerTest
 
         var ex = Assert.ThrowsAsync<ArgumentException>(() => _handler.Handle(new DeleteProjectCommand(1), It.IsAny<CancellationToken>()));
         Assert.That(ex.Message, Is.EqualTo("Project not found."));
+    }
+
+    [Test]
+    public void LogWhenProjectIsDeleted_Test()
+    {
+        var project = new Project
+        {
+            Id = 1,
+            ProjectName = "Heather",
+            ClientName = "Metatron",
+            BusinessUnit = "666",
+            Department = "Silent Hill",
+            TeamNumber = 3,
+            IsArchived = true
+        };
+        _mockProjectRepo.Setup(m => m.GetProjectAsync(It.IsAny<int>())).ReturnsAsync(project);
+        _mockProjectRepo.Setup(m => m.DeleteProjectAsync(It.IsAny<Project>())).ReturnsAsync(project);
+
+        _ = _handler.Handle(new DeleteProjectCommand(1), It.IsAny<CancellationToken>());
+
+        _mockLogRepo.Verify(m => m.AddProjectLogForCurrentUser(It.Is<Project>(
+                project => project.ProjectName == "Heather" && project.ClientName == "Metatron" &&
+                           project.BusinessUnit == "666" && project.Department == "Silent Hill" && project.TeamNumber == 3),
+            Action.REMOVED_PROJECT, It.Is<List<LogChange>>(
+                changes => changes.Any(change => change.Property == "ProjectName" && change.OldValue == "Heather" && change.NewValue == "")
+                           && changes.Any(change => change.Property == "ClientName" && change.OldValue == "Metatron" && change.NewValue == "")
+                           && changes.Any(change => change.Property == "BusinessUnit" && change.OldValue == "666" && change.NewValue == "")
+                           && changes.Any(change => change.Property == "Department" && change.OldValue == "Silent Hill" && change.NewValue == "")
+                           && changes.Any(change => change.Property == "TeamNumber" && change.OldValue == "3" && change.NewValue == ""))), Times.Once);
     }
 
 }
