58aa8b095821a95e823415a8d1c598e9e5c4e7e3
Finn Wulfert <finn.wulfert@stud.htwk-leipzig.de>
2024-12-07 14:54:53 +0100

    Fix Code Quality

--- ProjectMetadataPlatform.Application/Projects/GetProjectIdBySlugQuery.cs
+++ ProjectMetadataPlatform.Application/Projects/GetProjectIdBySlugQuery.cs
@@ -6,5 +6,4 @@ namespace ProjectMetadataPlatform.Application.Projects;
 /// Query for getting the Id of a project by its slug.
 /// <param name="Slug">The slug of the project to get the Id for.</param>
 /// </summary>
-/// <param name="Slug">Slug of the project.</param>
 public record GetProjectIdBySlugQuery(string Slug) : IRequest<int?>;
