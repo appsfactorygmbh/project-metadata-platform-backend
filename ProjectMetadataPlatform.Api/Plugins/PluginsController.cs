d0df2eb3cd2fef29c0fdbf31ccf331a8ba2ae8b5
Kilian Schneider <kilian.schneider@stud.htwk-leipzig.de>
2025-01-13 23:20:50 +0100

    return BaseUrl when GETing a global Plugin

--- ProjectMetadataPlatform.Api/Plugins/PluginsController.cs
+++ ProjectMetadataPlatform.Api/Plugins/PluginsController.cs
@@ -104,7 +104,7 @@ public class PluginsController : ControllerBase
             return NotFound("No Plugin with id " + pluginId + " was found.");
         }
 
-        var response = new GetGlobalPluginResponse(plugin.PluginName, plugin.Id, plugin.IsArchived, []);
+        var response = new GetGlobalPluginResponse(plugin.PluginName, plugin.Id, plugin.IsArchived, [], plugin.BaseUrl);
         return Ok(response);
     }
 
@@ -135,7 +135,9 @@ public class PluginsController : ControllerBase
             plugin.PluginName,
             plugin.Id,
             plugin.IsArchived,
-            keys));
+            keys,
+            plugin.BaseUrl
+            ));
 
         return Ok(response);
     }
