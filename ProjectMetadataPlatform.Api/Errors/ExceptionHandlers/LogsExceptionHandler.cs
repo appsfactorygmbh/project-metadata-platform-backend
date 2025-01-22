d3b188c32356defcc7ef65083ae10f41eb07a539
Konrad Lames <konrad.lames@stud.htwk-leizpig.de>
2025-01-22 10:55:58 +0100

    Missing XML docs.

--- ProjectMetadataPlatform.Api/Errors/ExceptionHandlers/LogsExceptionHandler.cs
+++ ProjectMetadataPlatform.Api/Errors/ExceptionHandlers/LogsExceptionHandler.cs
@@ -6,8 +6,16 @@ using ProjectMetadataPlatform.Domain.Errors.LogExceptions;
 
 namespace ProjectMetadataPlatform.Api.Errors.ExceptionHandlers;
 
+/// <summary>
+/// Handles exceptions related to logs in the Project Metadata Platform API.
+/// </summary>
 public class LogsExceptionHandler : ControllerBase, IExceptionHandler<LogException>
 {
+    /// <summary>
+    /// Handles a specific log exception and returns an appropriate HTTP response.
+    /// </summary>
+    /// <param name="exception">The project exception to handle.</param>
+    /// <returns>An IActionResult representing the result of handling the log exception.</returns>
     public IActionResult Handle(LogException exception)
     {
         Console.WriteLine(exception.Message);
--- ProjectMetadataPlatform.Domain/Errors/LogExceptions/LogActionNotSupportedException.cs
+++ ProjectMetadataPlatform.Domain/Errors/LogExceptions/LogActionNotSupportedException.cs
@@ -2,4 +2,8 @@
 
 namespace ProjectMetadataPlatform.Domain.Errors.LogExceptions;
 
+/// <summary>
+/// Exception thrown when a specific log action is not supported.
+/// </summary>
+/// <param name="action">The action that is not supported.</param>
 public class LogActionNotSupportedException(Action action) : LogException("The action " + action + " is not supported.");
