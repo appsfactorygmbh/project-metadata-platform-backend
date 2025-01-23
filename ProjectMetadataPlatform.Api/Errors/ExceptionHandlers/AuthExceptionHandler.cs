1c18248e88560714f978a418fed3adb6cbb31da1
Tristan Sopauschke <tristan.sopauschke@stud.htwk-leipzig.de>
2025-01-23 18:22:24 +0100

    Add AuthExceptionHandler

---
+++ ProjectMetadataPlatform.Api/Errors/ExceptionHandlers/AuthExceptionHandler.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectMetadataPlatform.Api.Interfaces;
+using ProjectMetadataPlatform.Domain.Errors.AuthExceptions;
+
+namespace ProjectMetadataPlatform.Api.Errors.ExceptionHandlers;
+
+public class AuthExceptionHandler: ControllerBase, IExceptionHandler<AuthException>
+{
+    public IActionResult? Handle(AuthException exception)
+    {
+        return exception switch
+        {
+            AuthInvalidLoginCredentialsException authInvalidLoginCredentialsException => BadRequest(authInvalidLoginCredentialsException.Message),
+            AuthInvalidRefreshTokenException authInvalidRefreshTokenException => BadRequest(authInvalidRefreshTokenException.Message),
+            _ => null
+        };
+    }
+}
