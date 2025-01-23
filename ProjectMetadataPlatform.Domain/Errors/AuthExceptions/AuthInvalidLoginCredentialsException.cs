e118fc8ba8b424536b3213bee4c594c07c601083
Tristan Sopauschke <tristan.sopauschke@stud.htwk-leipzig.de>
2025-01-23 18:26:56 +0100

    Add documentation

--- ProjectMetadataPlatform.Domain/Errors/AuthExceptions/AuthInvalidLoginCredentialsException.cs
+++ ProjectMetadataPlatform.Domain/Errors/AuthExceptions/AuthInvalidLoginCredentialsException.cs
@@ -1,3 +1,6 @@
 ﻿namespace ProjectMetadataPlatform.Domain.Errors.AuthExceptions;
 
+/// <summary>
+/// Exception thrown when invalid login credentials are provided.
+/// </summary>
 public class AuthInvalidLoginCredentialsException(): AuthException("Invalid login credentials.");
